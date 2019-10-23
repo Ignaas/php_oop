25deb4c8c7e7dfffd6064b39566b9f109a180ea3
Ignaas <54891210+Ignaas@users.noreply.github.com>
2019-10-23 09:26:21 +0300

    Užduotis #1

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -13,12 +13,45 @@ class FileDB {
         $this->data = file_to_array($this->file_name);
     }
 
+    public function getData() {
+        return $this->data;
+    }
+
     public function setData($data_array) {
         $this->data = $data_array;
     }
 
     public function save() {
         return array_to_file($this->data, $this->file_name);
+    }
+
+    public function getRow($table, $row_id) {
+        if (isset($this->data[$table])) {
+            return $this->data[$table][$row_id];
+        }
+    }
+
+    public function addRow($table, $row) {
+        $this->data[$table] = $row;
+    }
+
+    public function replaceRow($table, $row_id, $row) {
+        if (isset($this->data[$table][$row_id])) {
+            $this->data[$table][$row_id] = $row;
+        } else {
+            return false;
+        }
+    }
+
+    public function createTable($table_name) {
+        if (!isset($this->data[$table_name])) {
+            $this->data[$table_name] = [];
+
+            return true;
+        } else {
+
+            return false;
+        }
     }
 
 }
